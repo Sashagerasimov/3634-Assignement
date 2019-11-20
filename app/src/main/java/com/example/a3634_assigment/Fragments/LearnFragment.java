5a6c3f066a4a40cdb784755c60e7298d8033f77a
Rachel Jiang <rachel.jiang811@gmail.com>
2019-11-20 16:53:42 +1100

    cleaned code

--- app/src/main/java/com/example/a3634_assigment/Fragments/LearnFragment.java
+++ app/src/main/java/com/example/a3634_assigment/Fragments/LearnFragment.java
@@ -38,56 +38,61 @@ public class LearnFragment extends Fragment {
         levelUnlock = view.findViewById(R.id.currentLevelTextView);
         PlanetAdapter planetAdapter = new PlanetAdapter();
 
+        if(SessionInfo.currentUser.getScore()<100){
+            levelUnlock.setText("Earn "+ (100 - SessionInfo.currentUser.getScore()) + " more points to unlock Level Venus!");
+        }
+
+
         //unlocks levels according to user score
-        if(SessionInfo.currentUser.getScore()>=100) {
+        if(SessionInfo.currentUser.getScore()<100) {
             planetAdapter.setData(PlanetBank.getLevel1());
             recyclerView.setAdapter(planetAdapter);
-            levelUnlock.setText("Earn "+ (200 - SessionInfo.currentUser.getScore()) + " more points to unlock Level Venus!");
+            levelUnlock.setText("Earn "+ (100 - SessionInfo.currentUser.getScore()) + " more points to unlock Level Venus!");
 
+
+        }
+        if(SessionInfo.currentUser.getScore()>=100) {
+            planetAdapter.setData(PlanetBank.getLevel2());
+            recyclerView.setAdapter(planetAdapter);
+            levelUnlock.setText("Earn "+ (200 - SessionInfo.currentUser.getScore()) + " more points to unlock Level Earth!");
 
         }
         if(SessionInfo.currentUser.getScore()>=200) {
-            planetAdapter.setData(PlanetBank.getLevel2());
-            recyclerView.setAdapter(planetAdapter);
-            levelUnlock.setText("Earn "+ (300 - SessionInfo.currentUser.getScore()) + " more points to unlock Level Earth!");
-
-        }
-        if(SessionInfo.currentUser.getScore()>=300) {
             planetAdapter.setData(PlanetBank.getLevel3());
             recyclerView.setAdapter(planetAdapter);
-            levelUnlock.setText("Earn "+ (400 - SessionInfo.currentUser.getScore()) + " more points to unlock Level Mars!");
+            levelUnlock.setText("Earn "+ (300 - SessionInfo.currentUser.getScore()) + " more points to unlock Level Mars!");
         }
-        if(SessionInfo.currentUser.getScore()>=400) {
+        if(SessionInfo.currentUser.getScore()>=300) {
             planetAdapter.setData(PlanetBank.getLevel4());
             recyclerView.setAdapter(planetAdapter);
-            levelUnlock.setText("Earn "+ (500 - SessionInfo.currentUser.getScore()) + " more points to unlock Level Jupiter!");
+            levelUnlock.setText("Earn "+ (400 - SessionInfo.currentUser.getScore()) + " more points to unlock Level Jupiter!");
+
+        }
+        if(SessionInfo.currentUser.getScore()>=400) {
+            planetAdapter.setData(PlanetBank.getLevel5());
+            recyclerView.setAdapter(planetAdapter);
+            levelUnlock.setText("Earn "+ (500 - SessionInfo.currentUser.getScore()) + " more points to unlock Level Saturn!");
 
         }
         if(SessionInfo.currentUser.getScore()>=500) {
-            planetAdapter.setData(PlanetBank.getLevel5());
+            planetAdapter.setData(PlanetBank.getLevel6());
             recyclerView.setAdapter(planetAdapter);
-            levelUnlock.setText("Earn "+ (600 - SessionInfo.currentUser.getScore()) + " more points to unlock Level Saturn!");
+            levelUnlock.setText("Earn "+ (600 - SessionInfo.currentUser.getScore()) + " more points to unlock Level Uranus!");
 
         }
         if(SessionInfo.currentUser.getScore()>=600) {
-            planetAdapter.setData(PlanetBank.getLevel6());
+            planetAdapter.setData(PlanetBank.getLevel7());
             recyclerView.setAdapter(planetAdapter);
-            levelUnlock.setText("Earn "+ (700 - SessionInfo.currentUser.getScore()) + " more points to unlock Level Uranus!");
+            levelUnlock.setText("Earn "+ (700 - SessionInfo.currentUser.getScore()) + " more points to unlock Level Neptune!");
 
         }
         if(SessionInfo.currentUser.getScore()>=700) {
-            planetAdapter.setData(PlanetBank.getLevel7());
+            planetAdapter.setData(PlanetBank.getLevel8());
             recyclerView.setAdapter(planetAdapter);
-            levelUnlock.setText("Earn "+ (800 - SessionInfo.currentUser.getScore()) + " more points to unlock Level Neptune!");
+            levelUnlock.setText("Earn "+ (800 - SessionInfo.currentUser.getScore()) + " more points to unlock Level Pluto!");
 
         }
         if(SessionInfo.currentUser.getScore()>=800) {
-            planetAdapter.setData(PlanetBank.getLevel8());
-            recyclerView.setAdapter(planetAdapter);
-            levelUnlock.setText("Earn "+ (900 - SessionInfo.currentUser.getScore()) + " more points to unlock Level Pluto!");
-
-        }
-        if(SessionInfo.currentUser.getScore()>=900) {
             planetAdapter.setData(PlanetBank.getLevel9());
             recyclerView.setAdapter(planetAdapter);
             levelUnlock.setText("Congratulations! You have finished Solaris!");
